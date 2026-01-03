7d6a08d5a66259101d93d76f1ba701d17865f8aa
roma12el <romael2003@gmail.com>
2026-01-03 18:12:43 +0100

    Change data loading from Excel to CSV format

--- core/loader.py
+++ core/loader.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 def load_data(file):
-    df = pd.read_excel(file)
+    df = pd.read_csv(file)
     df.columns = df.columns.str.lower()
     df['date'] = pd.to_datetime(df['date'])
     return df.sort_values('date')
